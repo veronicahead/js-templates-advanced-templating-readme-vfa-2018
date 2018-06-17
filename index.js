13d1a1d17b05d847852e10c42994453399463d5d
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-17 01:24:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 function loadIssue() {
-  var issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
-  }
-  
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  var result = template(issue);
-  document.getElementsByTagName("main")[0].innerHTML += result;
+  for(var i=0;i<issues.length;i++) {
+    var result = template(issues[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
+  }
 }
